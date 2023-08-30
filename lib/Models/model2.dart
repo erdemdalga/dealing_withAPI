2a1b62f7647efe4ffacd4bc6eca451c1be82a7b0
pratyushvaibhav <pratyushvaibhaw@gmail.com>
2023-08-31 04:04:34 +0530

    1.0.1|Created a model class for simple non nested json object and displayed the data using future builder

---
+++ lib/Models/model2.dart
@@ -0,0 +1,7 @@
+// Creating a simple model class for non nested json object.
+class Photos {
+  int id;
+  String title;
+  String url;
+  Photos({required this.id, required this.title, required this.url});
+}
--- lib/homePage.dart
+++ lib/homePage.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 import 'dart:io';
 
+import 'package:api_course/Models/model2.dart';
 import 'package:api_course/Models/post_model.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
@@ -13,28 +14,25 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  List<Photos> photosList = [];
   List<PostsModel> postList = [];
-  Future<List<PostsModel>> getPostApi() async {
-    final response =
-        await http.get(Uri.parse('https://jsonplaceholder.typicode.com/posts'));
-    sleep(Duration(seconds: 20));
+  Future<List<Photos>> getPostApi() async {
+    final response = await http
+        .get(Uri.parse('https://jsonplaceholder.typicode.com/photos'));
     var data = jsonDecode(response.body.toString());
     if (response.statusCode == 200) {
-      for (Map<String, dynamic> i in data) postList.add(PostsModel.fromMap(i));
-      return postList;
-    } else {
-      return postList;
-    }
+      for (Map m in data) {
+        Photos photos = Photos(id: m['id'], title: m['title'], url: m['url']);
+        photosList.add(photos);
+      }
+      return photosList;
+    } else
+      return photosList;
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // bottomNavigationBar: BottomAppBar(
-      //   color: Color.fromARGB(255, 189, 234, 255),
-      //   height: 75,
-      //   elevation: 5,
-      // ),
       appBar: AppBar(
         elevation: 5,
         backgroundColor: Color.fromARGB(255, 189, 234, 255),
@@ -44,34 +42,74 @@ class _HomePageState extends State<HomePage> {
       ),
       body: Container(
         child: FutureBuilder(
-          builder: (context, snapshot) {
-            if (!snapshot.hasData)
-              return Center(
-                  child: CircularProgressIndicator(
-                backgroundColor: Color.fromARGB(255, 189, 234, 255),
-                color: Colors.red,
-              ));
-            else {
-              return ListView.builder(
-                itemBuilder: (context, index) {
-                  return Card(
-                    margin: EdgeInsets.all(10),
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(20)),
-                    child: ListTile(
-                      leading: CircleAvatar(child: Text('${index + 1}')),
-                      subtitle: Text(postList[index].body.toString()),
-                      title: Text(postList[index].title.toString()),
-                      trailing: Icon(Icons.arrow_forward_ios_rounded),
-                    ),
-                  );
-                },
-                itemCount: postList.length,
-              );
-            }
-          },
-          future: getPostApi(),
-        ),
+            future: getPostApi(),
+            builder: (context, AsyncSnapshot<List<Photos>> snapshot) {
+              if (!snapshot.hasData)
+                return Center(
+                    child: CircularProgressIndicator(
+                  backgroundColor: Colors.red,
+                  color: Color.fromARGB(255, 189, 234, 255),
+                ));
+              else {
+                return ListView.builder(
+                  itemBuilder: (context, index) {
+                    return Container(
+                      height: 100,
+                      padding: EdgeInsets.all(8),
+                      margin: EdgeInsets.all(10),
+                      child: ListTile(
+                        leading: CircleAvatar(
+                          child: Text(snapshot.data![index].id.toString()),
+                        ),
+                        title: Text(snapshot.data![index].title),
+                        trailing: Container(
+                          width: 40,
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(25),
+                            child: Image.network(snapshot.data![index].url),
+                          ),
+                        ),
+                      ),
+                    );
+                  },
+                  itemCount: 10,
+                );
+              }
+            }),
+      ),
+    );
+  }
+
+  Container post_modelBody() {
+    return Container(
+      child: FutureBuilder(
+        builder: (context, snapshot) {
+          if (!snapshot.hasData)
+            return Center(
+                child: CircularProgressIndicator(
+              backgroundColor: Color.fromARGB(255, 189, 234, 255),
+              color: Colors.red,
+            ));
+          else {
+            return ListView.builder(
+              itemBuilder: (context, index) {
+                return Card(
+                  margin: EdgeInsets.all(10),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20)),
+                  child: ListTile(
+                    leading: CircleAvatar(child: Text('${index + 1}')),
+                    subtitle: Text(postList[index].body.toString()),
+                    title: Text(postList[index].title.toString()),
+                    trailing: Icon(Icons.arrow_forward_ios_rounded),
+                  ),
+                );
+              },
+              itemCount: postList.length,
+            );
+          }
+        },
+        future: getPostApi(),
       ),
     );
   }
